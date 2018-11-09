9dd3dddfc7d8923812df8f324ab211b9dec310b2
Anikkenbg <anikkenbg@student.ie.edu>
2018-11-09 16:48:19 +0100

    Update assignment.py
    Bubble sort function

--- assignment.py
+++ assignment.py
@@ -6,3 +6,14 @@ Created on Fri Nov  9 16:30:06 2018
 @author: anikkengjeruldsen
 """
 
+# Bubble sort implementation.
+#
+# This function returns nothing because it works by mutating the lst.
+def bubble(lst):
+    for j in range(0, len(lst) - 1):
+        for i in range(0, len(lst) - j - 1):
+            if lst[i] > lst[i + 1]:
+                temp = lst[i]
+                lst[i] = lst[i + 1]
+                lst[i + 1] = temp
+                
